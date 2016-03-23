1ae15ef99d63c8fbd0fe25e5c5f462b885e775a0
siyue <sun.siyue@gmail.com>
2016-03-23 11:21:02 +0800

    1.3 two string permutation of each other

--- main.cpp
+++ main.cpp
@@ -17,14 +17,27 @@ using namespace std;
 
 int main() {
     // insert code here...
-    string s = "acbd";
-    int one=0, two=s.size()-1;
+    string s1 = "acbd";
+    string s2 = "dcba";
     
-    while (one < two) {
-        swap(s[one], s[two]);
-        one++;
-        two--;
+    
+    
+    unordered_map<char, int> map1;
+    unordered_map<char, int> map2;
+    
+    for (auto c:s1) {
+        map1[c] += 1;
     }
     
-    cout << s;
+    for (auto c:s2) {
+        map2[c] += 1;
+    }
+    
+    if (map1 == map2) {
+        cout << "equal\n";
+    } else {
+        cout << "no\n";
+    }
+    
+  
 }
